7c624ec3caa0dcca25f8d87677ca0e7eeeba2d98
onoto-studio <128829377+onoto-studio@users.noreply.github.com>
2025-04-23 20:10:30 +0200

    Update Bounded.tsx

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -20,12 +20,13 @@ export function Bounded({
     <Comp
       data-collapsible={collapsible}
       className={clsx(
-        "px-6",
-        yPadding === "sm" && "py-8 md:py-10",
-        yPadding === "base" && "py-20 md:py-28",
-        yPadding === "lg" && "py-32 md:py-48",
-        className,
-      )}
+  "px-4", // Marge horizontale plus petite
+  yPadding === "sm" && "py-4 md:py-6", // Marge verticale petite pour 'sm'
+  yPadding === "base" && "py-6 md:py-8", // Marge verticale encore plus petite pour 'base'
+  yPadding === "lg" && "py-8 md:py-12", // Marge verticale encore plus petite pour 'lg'
+  className,
+)}
+      
     >
       <div className="mx-auto w-full max-w-6xl">{children}</div>
     </Comp>
