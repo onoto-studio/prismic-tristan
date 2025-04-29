82f1566a2c84c3925bcd7437d6db1cd4046f6be9
onoto-studio <128829377+onoto-studio@users.noreply.github.com>
2025-04-29 12:29:34 +0200

    Update index.tsx

--- src/slices/Image/index.tsx
+++ src/slices/Image/index.tsx
@@ -17,11 +17,11 @@ const Image: FC<ImageProps> = ({ slice, index }) => {
   className={clsx("bg-white h-screen flex items-center justify-center", index === 0 && "pt-0 md:pt-0")}
 >
   {isFilled.image(image) && (
-    <div className="aspect-[3/2] w-full bg-gray-100 items-center justify-center">
+    <div className="aspect-[3/2] bg-gray-100 items-center justify-center">
       <PrismicNextImage
         field={image}
-        sizes="100vw"
-        className="w-full h-full object-fit"
+        sizes="50vw"
+        className="w-full h-full object-contain"
       />
     </div>
   )}
